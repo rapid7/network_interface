6b97e592f565405434b35013fc412dd96985eaa4
Dustin Eichler <84730+dustineichler@users.noreply.github.com>
2019-11-06 11:12:02 -0500

    Add class_methods.rb

---
+++ lib/network_interface/class_methods.rb
@@ -0,0 +1,24 @@
+require_relative 'finder'
+
+module NetworkInterface
+  module ClassMethods
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
+
+    def first
+      finder.first
+    end
+
+    def filter(q:)
+      query = -> address { address.include? q }
+      finder.find(&query)
+    end
+
+    private
+
+    def finder
+      Finder
+    end
+  end
+end
