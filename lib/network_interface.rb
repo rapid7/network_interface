44a3fb973c3bc2df4573c65f92ae968c537f64ed
Dustin Eichler <84730+dustineichler@users.noreply.github.com>
2019-11-06 11:11:03 -0500

    Update network_interface.rb - include ClassMethods

--- lib/network_interface.rb
+++ lib/network_interface.rb
@@ -1,6 +1,8 @@
 require 'network_interface/version'
+require 'network_interface/class_methods'
 
-module NetworkInterface  
+module NetworkInterface
+  include ClassMethods
 end
 
 require 'network_interface_ext'
